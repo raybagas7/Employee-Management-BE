bf4747da7a15993c70a30bdc3f18f5e3388f19e0
raybagas7 <raysam.gas@gmail.com>
2024-01-25 01:35:49 +0700

    feat(server): - add jwt strategy

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const config = require('./utils/config');
 const ClientError = require('./exceptions/ClientError');
+
+// users requirement
 const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
+const keys = require('@hapi/jwt/lib/keys');
 
 const init = async () => {
   const userService = new UsersService();
@@ -17,6 +21,26 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  // Add hapi/jwt plugin
+  await server.register([{ plugin: Jwt }]);
+
+  // Authentications Strategy
+  server.auth.strategy('employee_mng', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register([
