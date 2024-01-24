ba46ca626dc56e78dc1427119c1e481f7410760f
raybagas7 <raysam.gas@gmail.com>
2024-01-24 22:13:34 +0700

    feat(exceptions): - make an error response based on what happened to the data - ClientError for incorrect client failure - AuthenticationError for incorrect credential - AuthorizationError for unauthorized user to access data - InvariantError for business logic error - NotFoundError for some data that not found in the server error

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const config = require('./utils/config');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const server = Hapi.server({
@@ -11,6 +12,35 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof Error) {
+      if (response instanceof ClientError) {
+        const errorResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        errorResponse.code(response.statusCode);
+        return errorResponse;
+      }
+
+      if (!response.isServer) {
+        return h.continue;
+      }
+
+      const serverErrorResponse = h.response({
+        status: 'error',
+        message: 'Error, something happened to our server',
+      });
+
+      serverErrorResponse.code(500);
+      return serverErrorResponse;
+    }
+
+    return h.continue;
   });
 
   await server.start();
