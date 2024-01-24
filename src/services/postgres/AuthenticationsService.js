6b5da1ec0306227c5f8e7f6df0ab464e98f91f0e
raybagas7 <raysam.gas@gmail.com>
2024-01-25 01:58:23 +0700

    feat(authentications service): - make authentications service to create, verify and delete jwt token

--- src/services/postgres/AuthenticationsService.js
+++ src/services/postgres/AuthenticationsService.js
@@ -1,8 +1,40 @@
 const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
 
 class AuthenticationsService {
   constructor() {
     this._pool = new Pool();
+  }
+
+  async addRefreshToken(token, username) {
+    const query = {
+      text: 'UPDATE authentications SET token = $1 WHERE owner = $2',
+      values: [token, username],
+    };
+
+    await this._pool.query(query);
+  }
+
+  async verifyRefreshToken(token) {
+    const query = {
+      text: 'SELECT token FROM authentications WHERE token = $1',
+      values: [token],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new InvariantError('Refresh token are invalid');
+    }
+  }
+
+  async deleteRefreshToken(token, username) {
+    const query = {
+      text: 'UPDATE authentications SET token = null WHERE token = $1 AND owner = $2',
+      values: [token, username],
+    };
+
+    await this._pool.query(query);
   }
 }
 
